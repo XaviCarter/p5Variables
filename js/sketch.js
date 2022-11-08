8b9f478ba54233e1f1a54d86b734d141f2d4f30a
Xavi Carter <116882813+XaviCarter@users.noreply.github.com>
2022-11-08 10:01:15 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,23 @@
+var firstName = "Xavi";
+var stringvar = "String";
+var intvar = 17;
+var undefinedvar;
+var nullvar = null; 
+
+var ellipseX = 400;
+var ellipseY = 400;
+
 function setup() {
     var canvas = createCanvas(800, 800);
     canvas.parent("p5container");
-}
+    console.log(intvar);
+}   background(220);
 
 function draw() {
-    background(220);
+    
 
     fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    ellipse(mouseX, mouseY, 100);
+
+    console.log(width, height);
 }
